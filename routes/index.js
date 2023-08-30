2f18a4ff8e72636e661ea128ff6320a955f755b8
vldan <vlad.danilov.020212@gmail.com>
2023-08-30 16:04:25 +0300

    fix: Перенести добавление _id usera в app.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,6 @@ router.get("/", (req, res) => {
   res.send("Сервер работает и ждет запросы, все круто!");
 });
 
-router.use((req, res, next) => {
-  req.user = {
-    _id: "64ea7977954b395f456e2bc9", // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-  next();
-});
-
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
