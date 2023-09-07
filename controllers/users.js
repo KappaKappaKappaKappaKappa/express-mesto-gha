27b674644e28f0a2fe38ed96c66d28c5d0f9ccf0
vldan <vlad.danilov.020212@gmail.com>
2023-09-07 22:21:47 +0300

    refactor: Изменить .then на async/await в контроллере users

--- controllers/users.js
+++ controllers/users.js
@@ -11,128 +11,129 @@ const {
   STATUS_AUTH_ERROR,
 } = require("../utils/errors");
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.status(STATUS_OK).send({ data: users });
-    })
-    .catch((err) => {
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(STATUS_OK).send({ data: users });
+  } catch (err) {
+    res
+      .status(STATUS_SERVER_ERROR)
+      .send({ message: "Внутренняя ошибка сервера" });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      res
+        .status(STATUS_NOT_FOUND)
+        .send({ message: "Запрашиваемый пользователь не найден" });
+    } else {
+      res.status(STATUS_OK).send({ data: user });
+    }
+  } catch (err) {
+    if (err.name === "CastError") {
+      res
+        .status(STATUS_BAD_REQUEST)
+        .send({ message: "Переданы некорректные данные" });
+    } else {
       res
         .status(STATUS_SERVER_ERROR)
         .send({ message: "Внутренняя ошибка сервера" });
-    });
-};
-
-const getUser = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        res
-          .status(STATUS_NOT_FOUND)
-          .send({ message: "Запрашиваемый пользователь не найден" });
-      } else {
-        res.status(STATUS_OK).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res
-          .status(STATUS_BAD_REQUEST)
-          .send({ message: "Переданы некорректные данные" });
-      } else {
-        res
-          .status(STATUS_SERVER_ERROR)
-          .send({ message: "Внутренняя ошибка сервера" });
-      }
-    });
-};
-
-const createUser = (req, res) => {
-  const { name, about, avatar, email, password } = req.body;
-  if (password.length < 8) {
-    res
-      .status(STATUS_BAD_REQUEST)
-      .send({ message: "Пароль должен быть минимум 8 символов" });
-    return;
+    }
   }
-
-  bcrypt.hash(password, 10)
-  .then((hash) => {
-    User.create({ name, about, avatar, email, password: hash })
-      .then((user) => {
-        res.status(STATUS_CREATED).send({ data: user });
-      })
-      .catch((err) => {
-        if (err.name === "ValidationError") {
-          res
-            .status(STATUS_BAD_REQUEST)
-            .send({ message: "Переданы некорректные данные" });
-        } else {
-          res
-            .status(STATUS_SERVER_ERROR)
-            .send({ message: "Внутренняя ошибка сервера" });
-        }
-      });
-  });
 };
 
-const updateProfile = (req, res) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true }
-  )
-    .then((user) => {
-      if (!user) {
-        res
-          .status(STATUS_NOT_FOUND)
-          .send({ message: "Пользователь с указанным id не найден" });
-      } else {
-        res.status(STATUS_OK).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res
-          .status(STATUS_BAD_REQUEST)
-          .send({ message: "Переданы некорректные данные" });
-      } else {
-        res
-          .status(STATUS_SERVER_ERROR)
-          .send({ message: "Внутренняя ошибка сервера" });
-      }
+const createUser = async (req, res) => {
+  const { name, about, avatar, email, password } = req.body;
+  try {
+    if (password.length < 8) {
+      res
+        .status(STATUS_BAD_REQUEST)
+        .send({ message: "Пароль должен быть минимум 8 символов" });
+      return;
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
     });
+    res.status(STATUS_CREATED).send({ data: user });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res
+        .status(STATUS_BAD_REQUEST)
+        .send({ message: "Переданы некорректные данные" });
+    } else {
+      res
+        .status(STATUS_SERVER_ERROR)
+        .send({ message: "Внутренняя ошибка сервера" });
+    }
+  }
 };
 
-const updateAvatar = (req, res) => {
+const updateProfile = async (req, res) => {
+  const { name, about } = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true, runValidators: true }
+    );
+    if (!user) {
+      res
+        .status(STATUS_NOT_FOUND)
+        .send({ message: "Пользователь с указанным id не найден" });
+    } else {
+      res.status(STATUS_OK).send({ data: user });
+    }
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res
+        .status(STATUS_BAD_REQUEST)
+        .send({ message: "Переданы некорректные данные" });
+    } else {
+      res
+        .status(STATUS_SERVER_ERROR)
+        .send({ message: "Внутренняя ошибка сервера" });
+    }
+  }
+};
+
+
+const updateAvatar = async (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true }
-  )
-    .then((newAvatar) => {
-      if (!newAvatar) {
-        res
-          .status(STATUS_NOT_FOUND)
-          .send({ message: "Пользователь с указанным id не найден" });
-      } else {
-        res.status(STATUS_OK).send({ data: newAvatar });
-      }
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res
-          .status(STATUS_BAD_REQUEST)
-          .send({ message: "Переданы некорректные данные" });
-      } else {
-        res
-          .status(STATUS_SERVER_ERROR)
-          .send({ message: "Внутренняя ошибка сервера" });
-      }
-    });
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true, runValidators: true }
+    );
+    if (!user) {
+      res
+        .status(STATUS_NOT_FOUND)
+        .send({ message: "Пользователь с указанным id не найден" });
+    } else {
+      res.status(STATUS_OK).send({ data: user });
+    }
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res
+        .status(STATUS_BAD_REQUEST)
+        .send({ message: "Переданы некорректные данные" });
+    } else {
+      res
+        .status(STATUS_SERVER_ERROR)
+        .send({ message: "Внутренняя ошибка сервера" });
+    }
+  }
 };
 
 const login = async (req, res) => {
