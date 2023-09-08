4839c93250b204b68efa4dea5336e4a84a2f139e
vldan <vlad.danilov.020212@gmail.com>
2023-09-08 20:48:17 +0300

    fix: Добавить проверку формата email в Joi валидацию приходящих данный

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.use(
   "/signup",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
