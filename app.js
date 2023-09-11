42c3134fa3c47b43bc6808bcd385b08f907d2cc2
vldan <vlad.danilov.020212@gmail.com>
2023-09-11 12:10:14 +0300

    feat: Добавить в проект helmet и limiter для запросов

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const { errors } = require("celebrate");
 const { errorHandler } = require("./middlewares/errorHandler");
 
@@ -8,6 +10,14 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(helmet());
+app.use(limiter);
 
 app.use(express.json());
 app.use(cookieParser());
