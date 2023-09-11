36fb09ff3db6eebb012e970319537f3c5423bdba
vldan <vlad.danilov.020212@gmail.com>
2023-09-11 15:33:53 +0300

    fix: Исправить регулярное выражение для валидации ссылок

--- utils/validation.js
+++ utils/validation.js
@@ -1,4 +1,4 @@
-const urlRegex = /^(https?:\/\/)?(www\.)?([0-9a-z-]+\.[a-z]{2,})(\/[^\s]*)?$/i;
+const urlRegex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?/;
 
 module.exports = {
   urlRegex,
